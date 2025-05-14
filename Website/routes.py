cc4917923ddacf7691c7bcb4f24d252bb9e8512b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-15 10:24:43 +1200

    Got everything working and made a nother route for looking at characters.

--- Website/routes.py
+++ Website/routes.py
@@ -20,9 +20,17 @@ def all_characters():
     conn = sqlite3.connect("Dk.db")
     cur = conn.cursor()
     cur.execute('SELECT * FROM Character')
-    Character = cur.fetchall()
+    Characters = cur.fetchall()
     conn.close()
-    return render_template('all_characters.html', Character = Character)
+    return render_template('all_characters.html', Characters = Characters)
+
+@app.route("/Characters/<int:id>")
+def characters(id):
+    conn = sqlite3.connect("Dk.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Character WHERE id=?",(id,))
+    Character = cur.fetchone()
+    return render_template('character.html', Character = Character)
 
 
 if __name__ == "__main__":
