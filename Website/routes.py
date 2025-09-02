46c0854cd7b53895e6ddc9dbf8670d1298593644
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-09-03 10:26:30 +1200

    made pep 8 format changes

--- Website/routes.py
+++ Website/routes.py
@@ -1,25 +1,33 @@
 from flask import Flask, render_template
 import sqlite3
 # pip install flask
+# pepe 8 needed!
+# code coments!
+# testing :(!
+
 app = Flask(__name__)
 
 
+# This starting point of the website.
 @app.route('/')
 def home():
     return render_template("home.html")
 
 
+# This is for when you go to mutch ups and it shows you all characters in game
+# And you choose the one you want to see.
 @app.route("/all_characters")
 def all_characters():
     conn = sqlite3.connect("Dk.db")
     cur = conn.cursor()
     cur.execute('SELECT * FROM Character')
-    # Fetch all characters from the database
     Characters = cur.fetchall()
     conn.close()
     return render_template('all_characters.html', Characters=Characters)
 
 
+# This is for when you search for somthing that does not exist.
+# It lets you see this nice screen instead of a messy screen.
 @app.errorhandler(404)
 def page_not_found(Error):
     return render_template("404.html"), 404
@@ -27,17 +35,18 @@ def page_not_found(Error):
 
 @app.errorhandler(500)
 def special_exeption_handler(Error):
-    # This is a special exception handler for 500 errors
-    # You can log the error or perform other actions here
     print(f"An error occurred: {Error}")
     return render_template("500.html"), 500
 
 
+# When clicking on a character in the match ups section it takes you here.
+# All the info you need is taken from the data bace and showed to you.
 @app.route("/Characters/<int:id>")
 def characters(id):
     conn = sqlite3.connect("Dk.db")
     cur = conn.cursor()
-    cur.execute("SELECT name, How_to_play_matchup,Pdko_percent_ps2,Apdk_percent_ps2 FROM Character WHERE id=?", (id,))
+    cur.execute(
+            "SELECT name, How_to_play_matchup,Pdko_percent_ps2,Apdk_percent_ps2 FROM Character WHERE id=?", (id,))
     conn.commit()
     Character = cur.fetchone()
     return render_template('character.html', Character=Character)
